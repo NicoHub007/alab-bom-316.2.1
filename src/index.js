96fb93a12c015e17b717b8fd8e4c105be4a93c28
Yannick <ymulyangote@gmail.com>
2024-11-04 14:12:04 -0600

    Updated Project

--- src/index.js
+++ src/index.js
@@ -1,18 +1,38 @@
 // Create your game here!
 const maxTries = 5;
-function startGame(){
-    let userNum = prompt("Please enter a number from 1 - 10");
-    //console.log(userNum);
-    const answer = Math.floor(Math.random()*10);
-    //console.log(answer);
-    if (userNum === answer){
-        alert("You guessed it right!");
-    }
-    else if (userNum > answer){
-        alert("Your guess is too high");
-    }
-    else {
-        alert("Your guess is too low");
+function startGame() {
+    const answer = Math.floor(Math.random() * 10);
+    // promptUser();
+    let numTries = 0;
+    while (numTries < maxTries) {
+       // promptUser(answer);
+        let answered = promptUser(answer);
+        if (!answered){
+            break;
+        }
+        numTries++;
+        console.log("User is on try " + numTries);
     }
 }
 
+function promptUser(answer) {
+    let userNum = prompt("Please enter a number from 1 - 10");
+    //console.log(userNum);
+
+    //console.log(answer);
+    if (userNum !== null) {
+        if (userNum === answer) {
+            alert("You guessed it right!");
+        }
+        else if (userNum > answer) {
+            alert("Your guess is too high");
+        }
+        else if (userNum < answer) {
+            alert("Your guess is too low");
+        }
+    }
+    else {
+        alert("You did not guess anything");
+        return false;
+    }return true;
+}
